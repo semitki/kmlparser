cf9fd4a6ef72b37214887cac7b5e3b798e75bf72
Ernesto Celis <developer@celisdelafuente.net>
2018-05-07 21:09:04 -0500

    MErge migrations feature back

--- sql/coveredby.sql
+++ sql/coveredby.sql
@@ -1,4 +1,4 @@
-create or replace function update_seccion_manzanas(manzana_id integer, seccion_gid integer) returns integer as $$
+create or replace function update_seccion_manzanas(manzana_id integer, seccion_id integer) returns integer as $$
 BEGIN
   if (
     select st_coveredby(manzana, seccion) as maninsec
@@ -6,18 +6,18 @@ BEGIN
       select st_buffer(
         (select geom
           from manzanas_geo where id=manzana_id
-        ), 0.1) as manzana,
+        ), 1) as manzana,
       st_buffer(
         (select geom
-          from secciones_geo where gid = seccion_gid
-        ), 0.1) as seccion
+          from secciones_geo where id = seccion_id
+        ), 1.5) as seccion
     ) as foo
   )
   then
-    update manzanas_geojson set seccion = seccion_gid where id = manzana_id;
+    update manzanas_geojson set seccion = seccion_id where id = manzana_id;
     return 1;
   else
-    update manzanas_geojson set seccion = null where id = manzana_id;
+    select 'no';
     return 0;
   end if;
 END;
